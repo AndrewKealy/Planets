34a1cd1bba7d2b266b67940b35415dc80c6f7ae4
Andrew Kealy <andrewkealy1@gmail.com>
2021-11-17 12:00:17 +0000

    Initial Commit

---
+++ Planets/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Planets
+//
+//  Created by Andrew Kealy on 17/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Planets/PlanetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetsApp.swift
+//  Planets
+//
+//  Created by Andrew Kealy on 17/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct PlanetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
